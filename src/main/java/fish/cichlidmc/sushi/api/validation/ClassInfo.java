d3aa76a8d82060720e6211909e6aae27494bc7d2
TropheusJ <jverrellijr@icloud.com>
2025-09-06 18:58:16 -0400

    add fields to validation

--- src/main/java/fish/cichlidmc/sushi/api/validation/ClassInfo.java
+++ src/main/java/fish/cichlidmc/sushi/api/validation/ClassInfo.java
@@ -1,12 +1,20 @@
 package fish.cichlidmc.sushi.api.validation;
 
+import java.lang.constant.ClassDesc;
 import java.lang.constant.MethodTypeDesc;
 import java.lang.reflect.AccessFlag;
 import java.util.Optional;
 import java.util.Set;
 
 public interface ClassInfo {
+	/**
+	 * @return the superclass of this class. Empty for interfaces, primitives, and {@link Object}.
+	 */
+	Optional<ClassDesc> superclass();
+
 	Set<AccessFlag> flags();
 
 	Optional<MethodInfo> findMethod(String name, MethodTypeDesc desc);
+
+	Optional<FieldInfo> findField(String name, ClassDesc type);
 }
--- src/main/java/fish/cichlidmc/sushi/impl/validation/RuntimeClassInfo.java
+++ src/main/java/fish/cichlidmc/sushi/impl/validation/RuntimeClassInfo.java
@@ -1,17 +1,26 @@
 package fish.cichlidmc.sushi.impl.validation;
 
+import fish.cichlidmc.sushi.api.util.ClassDescs;
 import fish.cichlidmc.sushi.api.validation.ClassInfo;
+import fish.cichlidmc.sushi.api.validation.FieldInfo;
 import fish.cichlidmc.sushi.api.validation.MethodInfo;
 
 import java.lang.constant.ClassDesc;
 import java.lang.constant.MethodTypeDesc;
 import java.lang.reflect.AccessFlag;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 public record RuntimeClassInfo(RuntimeValidation validation, Class<?> clazz) implements ClassInfo {
+	@Override
+	public Optional<ClassDesc> superclass() {
+		return Optional.ofNullable(this.clazz.getSuperclass()).map(ClassDescs::of);
+	}
+
 	@Override
 	public Set<AccessFlag> flags() {
 		return this.clazz.accessFlags();
@@ -38,11 +47,39 @@ public record RuntimeClassInfo(RuntimeValidation validation, Class<?> clazz) imp
 			paramArray[i] = paramClass;
 		}
 
-		try {
-			Method method = this.clazz.getDeclaredMethod(name, paramArray);
-			return method.getReturnType() == returnType ? Optional.of(new RuntimeMethodInfo(method)) : Optional.empty();
-		} catch (NoSuchMethodException ignored) {
+		for (Method method : this.clazz.getDeclaredMethods()) {
+			if (!method.getName().equals(name))
+				continue;
+
+			if (!Arrays.equals(paramArray, method.getParameterTypes()))
+				continue;
+
+			if (returnType != method.getReturnType())
+				continue;
+
+			return Optional.of(new RuntimeMethodInfo(method));
+		}
+
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<FieldInfo> findField(String name, ClassDesc type) {
+		Class<?> typeClass = this.validation.findClass(type).map(RuntimeClassInfo::clazz).orElse(null);
+		if (typeClass == null) {
 			return Optional.empty();
 		}
+
+		for (Field field : this.clazz.getDeclaredFields()) {
+			if (!field.getName().equals(name))
+				continue;
+
+			if (typeClass != field.getType())
+				continue;
+
+			return Optional.of(new RuntimeFieldInfo(field));
+		}
+
+		return Optional.empty();
 	}
 }
